f179c5bfc5971fb99a57d6a4a3fd9cb25dae4d54
Britanny <britanny.moreno834@std.uni.edu.ni>
2025-06-24 16:29:22 -0600

    Cambio confirmado

--- holamundo.java
+++ holamundo.java
@@ -2,7 +2,7 @@ public class holamundo {
 public static void main(String[] args) {
 String nombre = "Estudiante";
 int a = 10;
-int b = 1;
+int b = 0;
 int suma = a + b;
 System.out.println("Hola, " + nombre + "!");
 System.out.println("La suma de " + a + " + " + b + " es: " + suma);
