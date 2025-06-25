6a2cc09bd77eb0c287f9d5a6d518b63b481b8693
Britanny <britanny.moreno834@std.uni.edu.ni>
2025-06-25 14:57:08 -0600

    Cambio saludo

--- holamundo.java
+++ holamundo.java
@@ -6,5 +6,6 @@ int b = 0;
 int suma = a + b;
 System.out.println("Hola, " + nombre + "!");
 System.out.println("La suma de " + a + " + " + b + " es: " + suma);
+System.out.println("Mi nombre es Britanny Isabella Moreno Sandino y mi número de carnet es 2025-2684U");
 }
 }
